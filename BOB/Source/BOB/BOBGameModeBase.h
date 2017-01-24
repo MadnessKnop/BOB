66206e3081ae7f41615fbaaae8787b7ec8233186
OLLIS-LABTOP\okram <o.kramm@hotmail.de>
2017-01-24 16:56:32 +0100

    creat Unreal c++ Projekt

---
+++ BOB/Source/BOB/BOBGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "BOBGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOB_API ABOBGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};

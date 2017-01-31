f4c9dbf5f7676b4c37d146f4da521118d3670527
OLLIS-LABTOP\okram <o.kramm@hotmail.de>
2017-01-31 17:35:23 +0100

    creat mainmenulevel and level one - worked on own vector

--- BOB/Source/BOB/OwnVector.cpp
+++ BOB/Source/BOB/OwnVector.cpp
@@ -5,8 +5,18 @@
 
 OwnVector::OwnVector()
 {
+	float x = 0;
+	float y = 0;
+	float z = 0;
 }
 
 OwnVector::~OwnVector()
 {
+}
+
+OwnVector::OwnVector(float xi, float yi, float zi)
+{
+	float xi = x;
+	float yi = y;
+	float zi = z;
 }
--- BOB/Source/BOB/OwnVector.h
+++ BOB/Source/BOB/OwnVector.h
@@ -1,13 +1,26 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
+#include "UnrealMathUtility.h"
 
 /**
  * 
  */
 class BOB_API OwnVector
 {
+	float x;
+	float y;
+	float z;
+public:
+	// z vector (0,0,1)
+	static CORE_API const OwnVector UpVector;
+	// x vector (1,0,0)
+	static CORE_API const OwnVector ForwardVector;
+	// y vectot (0,1,0)
+	static CORE_API const OwnVector RightVector;
+
 public:
 	OwnVector();
 	~OwnVector();
+	OwnVector(float xi, float yi, float zi);
 };
